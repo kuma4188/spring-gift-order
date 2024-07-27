9a4b30e2c88d62b20af197e3d1ba6f03fb6a466b
kuma4188 <psb4188@gmail.com>
2024-07-27 20:45:08 +0900

    refactor : api controller에 오퍼레이션 추가

--- src/main/java/gift/controller/api/OrderController.java
+++ src/main/java/gift/controller/api/OrderController.java
@@ -3,6 +3,8 @@ package gift.controller.api;
 import gift.dto.OrderDTO;
 import gift.dto.KakaoUserDTO;
 import gift.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/orders")
+@Tag(name = "Order API", description = "주문 관련 API")
 public class OrderController {
 
     private final OrderService orderService;
@@ -21,6 +24,7 @@ public class OrderController {
     }
 
     @PostMapping("/order/{wishlistId}")
+    @Operation(summary = "주문 생성", description = "위시리스트 ID를 사용하여 주문을 생성합니다.")
     public ResponseEntity<OrderDTO> placeOrder(@PathVariable("wishlistId") Long wishlistId, HttpSession session) {
         KakaoUserDTO kakaoUserDTO = (KakaoUserDTO) session.getAttribute("kakaoUserDTO");
         String accessToken = (String) session.getAttribute("accessToken");
